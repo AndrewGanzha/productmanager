1dd97f1a2f194746ac248d079c44f5b1b98fc557
Andrew Ganzha <jackbrikman@mail.ru>
2025-11-06 15:20:40 +0300

    Add sanctum for project

---
+++ backend/app/app/Http/Controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Nette\Schema\ValidationException;
+
+class LoginController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return response()->json(['message' => __('Welcome!')]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => __('The provided credentials do not match our records.'),
+        ]);
+    }
+}
